6808e7f9138167323b6024d7120ab7b2e8230fe5
MaiMeselhi <Mai.Meselhi@gmail.com>
2021-10-14 08:15:07 +0200

    create toggleDone method

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Todo} from './../../models/todo'
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+title = "todos";
+todos : Todo[]
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+
+    ]
   }
+
+  toggleDone (id:number) {
+    this.todos.map((todo, i) => {
+      if (i == id)todo.completed = !todo.completed;
+
+      return todo;
+    })
+  }
+
+  deleteTodo (id:number) {
+    this.todos = this.todos.filter((todo, i) => i !== id);
+  }
+
+
+
+
 
 }
